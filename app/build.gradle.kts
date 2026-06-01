ab1b0fcc78cf1531e91b5757b5de9b5e876a01d7
819christ <christtchiakpe819@gmail.com>
2026-06-01 15:03:32 +0100

    Add material library dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.material)
 
     // Room
     implementation(libs.androidx.room.runtime)
